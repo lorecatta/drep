3f272f39f4593416316e2733bf54fa4d4d96a46d
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-02-22 13:14:59 +0000

    Works fine

--- use.R
+++ use.R
@@ -1,5 +1,18 @@
 devtools::load_all()
 
+# load packages
+
+#library(dplyr)
+
+
+# Define parameters -----------------------------------------------------------
+
+
+l_lim <- seq(0, 95, 5)
+u_lim <- seq(5, 100, 5)
+phis <- c(1, 1, 1, 1)
+
+
 
 # Simulate some data ----------------------------------------------------------
 
@@ -10,19 +23,11 @@ x <- sample(1:50, age_groups, replace = TRUE)
 n_j <- x / sum(x)
 pop <- 500000
 
-l_lim <- seq(0, 95, 5)
-u_lim <- seq(5, 100, 5)
-phis <- c(1, 1, 1, 1)
-
 
 # Run a test ------------------------------------------------------------------
 
 
-incids <- calculate_incidences(FOI, u_lim, l_lim)
-
-infs <- vapply(incids, incidences_to_numbers, numeric(1), n_j, pop)
-
-R0 <- calculate_R0(FOI, pop, infs, phis)
+R0 <- calculate_R0(FOI, pop, n_j, u_lim, l_lim, phis)
 
 R0
 
@@ -38,16 +43,70 @@ n_j <- age_structure[age_structure$ID_0 == FOI_id0, 2:ncol(age_structure)]
 
 pop <- foi[4, "population"]
 
-incids <- calculate_incidences(FOI, u_lim, l_lim)
-
-infs <- vapply(incids, incidences_to_numbers, numeric(1), n_j, pop)
-
-R0 <- calculate_R0(FOI, pop, infs, phis)
+R0 <- calculate_R0(FOI, pop, n_j, u_lim, l_lim, phis)
 
 R0
 
 
-# ------------------------------------
+# with multiple FOI values ----------------------------------------------------
+
+
+# FOI equals zero is not allowed in R0 calculation
+foi_pos <- foi[foi[, "type"] != "pseudoAbsence", ]
+
+all_FOI_R0 <- calculate_R0_foi_bulk(foi_data = foi_pos,
+                                    age_data = age_structure,
+                                    id_0_field = "ID_0",
+                                    foi_field = "FOI",
+                                    pop_field = "population",
+                                    u_lim = u_lim,
+                                    l_lim = l_lim,
+                                    phis = phis)
+
+all_FOI_R0
+
+
+# with multiple combinations of infectiousness --------------------------------
+
+
+w_1 <- 0.45
+w_2 <- 0.85
+w_3 <- 0.15
+
+sym_2x_asym_comb <- calculate_infectiousness_sym_2x_asym(w_1, w_2, w_3)
+
+infectiousness_combs <- list(c(1, 1, 0, 0),
+                             c(1, 1, 1, 1),
+                             sym_2x_asym_comb)
+
+all_phis_R0 <- calculate_R0_infectiouss_bulk(phi_combs = infectiousness_combs,
+                                             FOI = FOI,
+                                             N = pop,
+                                             n_j = n_j,
+                                             u_lim = u_lim,
+                                             l_lim = l_lim)
+
+all_phis_R0
+
+
+# with multiple FOI AND combinations of infectiousness ------------------------
+
+
+all_phis_and_FOI_R0 <- calculate_R0_foi_and_infectiouss_bulk(foi_data = foi_pos,
+                                                             phi_combs = infectiousness_combs,
+                                                             age_data = age_structure,
+                                                             id_0_field = "ID_0",
+                                                             foi_field = "FOI",
+                                                             pop_field = "population",
+                                                             u_lim = u_lim,
+                                                             l_lim = l_lim)
+
+all_phis_and_FOI_R0
+
+
+
+
+# calculate cases -------------------------------------------------------------
 
 
 table1 <- readRDS("R0_r_all_squares_2.rds")
