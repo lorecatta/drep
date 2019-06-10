2565e8747a6041d64902865e7f69d17031d35a80
Lorenzo Cattarino <lcattari@dide.local>
2019-06-10 12:13:11 +0100

    update roxygen documentation

--- R/calculating_R0.R
+++ R/calculating_R0.R
@@ -1,7 +1,12 @@
-#' Calculate the incidence of primary, secondary, tertiary and quaternary dengue
-#' infections in a population, for a given force of infection. \emph{Incidence}
-#' is the probability that someone has a primary (or secondary, etc..) dengue
-#' infection in a particular age group.
+
+# -----------------------------------------------------------------------------
+
+# calculate_incidences
+
+#' \code{calculate_incidences} calculates the incidence of primary, secondary,
+#' tertiary and quaternary dengue infections in a population, for a given
+#' force of infection. \emph{Incidence} is the probability that someone has a
+#' primary (or secondary, etc..) dengue infection in a particular age group.
 #'
 #' @title Calculate the incidence of primary, secondary, tertiary and quaternary
 #'  dengue infections.
@@ -31,7 +36,10 @@
 #' calculate_incidences(FOI, a, b)
 #'
 #' @export
+
+
 calculate_incidences <- function(FOI, u_lim, l_lim) {
+
   if (FOI <= 0) {
     stop("FOI must be greater than zero")
 
@@ -65,9 +73,10 @@ calculate_incidences <- function(FOI, u_lim, l_lim) {
 
 # -----------------------------------------------------------------------------
 
+# incidences_to_numbers
 
-#' Calculate the \emph{number} of people (infected or displaying disease symptoms)
-#' from \emph{incidence} estimates.
+#' \code{incidences_to_numbers} calculates the \emph{number} of people
+#' (infected or displaying disease symptoms) from \emph{incidence} estimates.
 #'
 #' @title Convert incidences into numbers.
 #'
@@ -82,7 +91,10 @@ calculate_incidences <- function(FOI, u_lim, l_lim) {
 #' @return Numeric.
 #'
 #' @export
+
+
 incidences_to_numbers <- function(incidences, N, n_j) {
+
   ret <- incidences * N * n_j * 4
 
   sum(ret)
@@ -92,10 +104,11 @@ incidences_to_numbers <- function(incidences, N, n_j) {
 
 # -----------------------------------------------------------------------------
 
+# calculate_R0
 
-#' Calculate the dengue reproduction number using the at-equilibrium number of
-#' primary, secondary, tertiary and quaternary infections in a population and
-#' their relative infectiousness.
+#' \code{calculate_R0} calculates the dengue reproduction number using the
+#' at-equilibrium number of primary, secondary, tertiary and quaternary infections
+#' in a population and their relative infectiousness.
 #'
 #' @title Calculate the Dengue reproduction number (\eqn{R_0})
 #'
@@ -110,6 +123,7 @@ incidences_to_numbers <- function(incidences, N, n_j) {
 #'
 #' @export
 calculate_R0 <- function(FOI, N, n_j, u_lim, l_lim, phis) {
+
   incids <- calculate_incidences(FOI, u_lim, l_lim)
 
   infs <- vapply(incids, incidences_to_numbers, numeric(1), N, n_j)
@@ -121,174 +135,34 @@ calculate_R0 <- function(FOI, N, n_j, u_lim, l_lim, phis) {
 
 # -----------------------------------------------------------------------------
 
+# calculate_infectiousness_sym_2x_asym
 
-#' Wrapper for repeating calculation of the dengue reproduction number over
-#' multiple force of infection values.
-#'
-#' @title Calculate R0 for multiple force of infection values.
-#'
-#' @param foi_data A matrix with three columns. One contains the GADM adm 0
-#' unique identifier for the country of the force of infection data point. One
-#' contains the force of infection estimates. One contains the values of population
-#' size for the location to which the force of infection refers.
-#'
-#' @param age_data A matrix with the age structure of the different force of
-#'  infections in \code{foi_data}. Each row refers to an individual force of
-#'  infection estimate. The first column is the GADM adm 0 unique identifier for
-#'  the country of the force of infection data point. The remaining columns
-#'  contain the proportions of individuals in each age group.
-#'
-#' @param id_0_field A character string of the name of the column where the
-#'  GADM adm 0 unique identifier for the country is stored. It has to be the same
-#'  in the foi_data and the age_data datasets.
-#'
-#' @param foi_field A character string of the force of infection column name.
-#'
-#' @param pop_field A character string of the population size column name.
-#'
-#' @inheritParams calculate_incidences
-#'
-#' @inheritParams calculate_R0
-#'
-#' @return A numeric vector of length equals \code{nrow(foi_data)}.
-#'
-#' @export
-calculate_R0_foi_bulk <- function(foi_data,
-                                  age_data,
-                                  id_0_field,
-                                  foi_field,
-                                  pop_field,
-                                  u_lim,
-                                  l_lim,
-                                  phis) {
-  wrapper <- function(i) {
-    n_j <-
-      age_data[age_data[, id_0_field] == foi_data[i, id_0_field], 2:ncol(age_data)]
-
-    FOI <- foi_data[i, foi_field]
-
-    N <- foi_data[i, pop_field]
-
-    calculate_R0(FOI, N, n_j, u_lim, l_lim, phis)
-
-  }
-
-  n <- nrow(foi_data)
-
-  vapply(seq_len(n), wrapper, numeric(1))
-
-}
-
-
-# -----------------------------------------------------------------------------
-
-
-#' Wrapper for repeating calculation of the dengue reproduction number over
-#' multiple infectiousness combinations.
-#'
-#' @title Calculate R0 for multiple infectiousness combinations.
-#'
-#' @param phi_combs A list with different combinations of infectiousness
-#' of the four dengue infection.
-#'
-#' @inheritParams calculate_R0
-#'
-#' @return A numeric vector.
-#'
-#' @export
-calculate_R0_infectiouss_bulk <-
-  function(phi_combs, FOI, N, n_j, u_lim, l_lim) {
-    wrapper <- function(i) {
-      calculate_R0(
-        FOI = FOI,
-        N = N,
-        n_j = n_j,
-        u_lim = u_lim,
-        l_lim = l_lim,
-        phis = i
-      )
-
-    }
-
-    vapply(phi_combs, wrapper, numeric(1))
-
-  }
-
-
-# -----------------------------------------------------------------------------
-
-
-#' Wrapper for repeating calculation of the dengue reproduction number over
-#' multiple force of infection values and combination of infectiousness.
-#'
-#' @title Calculate R0 for multiple force of infection values and infectiousness
-#'  combinations.
-#'
-#' @inheritParams calculate_R0_foi_bulk
-#'
-#' @inheritParams calculate_R0_infectiouss_bulk
-#'
-#' @return A \emph{n} x \emph{m} matrix where \emph{n} is the number of force
-#' of infection estimates and emph{m} is the number of infectiousness combinations.
-#'
-#' @export
-calculate_R0_foi_and_infectiouss_bulk <- function(foi_data,
-                                                  phi_combs,
-                                                  age_data,
-                                                  id_0_field,
-                                                  foi_field,
-                                                  pop_field,
-                                                  u_lim,
-                                                  l_lim) {
-  n <- nrow(foi_data)
-
-  wrapper_1 <- function(i, phis) {
-    n_j <-
-      age_data[age_data[, id_0_field] == foi_data[i, id_0_field], 2:ncol(age_data)]
-
-    FOI <- foi_data[i, foi_field]
-
-    pop <- foi_data[i, pop_field]
-
-    calculate_R0(FOI, pop, n_j, u_lim, l_lim, phis)
-
-  }
-
-  wrapper_2 <- function(j) {
-    phis <- j
-
-    vapply(seq_len(n), wrapper_1, numeric(1), phis)
-
-  }
-
-  vapply(phi_combs, wrapper_2, numeric(n))
-
-}
-
-
-# -----------------------------------------------------------------------------
-
-
-#' Calculate the infectiousness of primary, secondary, tertiary and quaternary
-#' assuming that symptomatic infections are twice as infecious as asymptomatic
-#' ones. Infectiousness of secondary infectious is 1.
+#' \code{calculate_infectiousness_sym_2x_asym} calculates the infectiousness of
+#' primary, secondary, tertiary and quaternary assuming that symptomatic infections
+#' are twice as infecious as asymptomatic ones.
+#' Infectiousness of secondary infectious is 1.
 #'
 #' @title Calculate infectiousness when symptomatic infections are twice as
 #'  infectious as asymptomatic ones.
 #'
-#' @param w_1 proportions of primary infections which are symptomatic.
-#'
-#' @param w_2 proportions of secondary infections which are symptomatic.
-#'
-#' @param w_3 proportions of tertiary and quaternary infections which are symptomatic.
+#' @param prop_sym_parms numeric vector of proportions of primary, secondary and
+#' tertiary/quaternary infections which are symptomatic.
 #'
 #' @return Numeric.
 #'
 #' @export
-calculate_infectiousness_sym_2x_asym <- function(w_1, w_2, w_3) {
+
+
+calculate_infectiousness_sym_2x_asym <- function(prop_sym_parms) {
+
+  w_1 <- prop_sym_parms[1]
+  w_2 <- prop_sym_parms[2]
+  w_3 <- prop_sym_parms[3]
+
   phi_2 <- 1
   phi_1 <- (w_1 * 2 + (1 - w_1)) / (w_2 * 2 + (1 - w_2))
-  phi_3 <- phi_4 <- (w_3 * 2 + (1 - w_3)) / (w_2 * 2 + (1 - w_2))
+  phi_3 <- (w_3 * 2 + (1 - w_3)) / (w_2 * 2 + (1 - w_2))
 
-  c(phi_1, phi_2, phi_3, phi_4)
+  c(phi_1, phi_2, phi_3, phi_3)
+
 }
