7815927b831b6624dff311ed28c73c15e682914e
Lorenzo Cattarino <lcattari@dide.local>
2019-06-18 14:49:50 +0100

    create new foi dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,6 +4,6 @@ foi <- read.csv(file.path("data-raw", "foi.csv"))
 
 age_structure <- read.csv(file.path("data-raw", "age_structure.csv"))
 
-usethis::use_data(foi)
+usethis::use_data(foi, overwrite = TRUE)
 
-usethis::use_data(age_structure)
+usethis::use_data(age_structure, overwrite = TRUE)
