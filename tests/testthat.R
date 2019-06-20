98b07fc705f08db4c4b6228fcb95799984fd9ac3
Lorenzo Cattarino <lcattari@dide.local>
2019-06-20 14:43:38 +0100

    create tests/testthat and add test data

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(drep)
+
+test_check("drep")
---
+++ tests/testthat/test-R0-and-burden-measures.R
@@ -0,0 +1,6 @@
+test_that("R0 and burden outputs are correct", {
+
+  correct_outputs <- read.delim(file.path("test_data", "R0_and_burden_calculation_test.txt"))
+
+
+})
